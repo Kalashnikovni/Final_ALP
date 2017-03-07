1764f4e33142e0b601b0db61084950269c66027e
Kalashnikovni <denise.marzorati@gmail.com>
2017-03-06 22:15:20 -0300

    Actualiza estructuras de datos

--- Common.hs
+++ Common.hs
@@ -1,14 +1,17 @@
 module Common where
 
-type MyPoint       = (Float, Float)
+-- Tipos para polígonos desde línea de comando --
+-------------------------------------------------
 
-data Def           = Dp String Polygon Float -- ID, def. del polígono y escalamiento 
-                     | Dc String Container Float deriving Show
+type MyPoint       = (Float, Float)
 
 newtype Polygon    = P [MyPoint] deriving Show
 
 newtype Polygons   = Ps [Polygon] deriving Show
 
+-- Representamos un rectángulo de acuerdo a su diagonal "de izquierda a derecha, de abajo hacia arriba". 
+-- Es decir, con un vector del cuadrante I.
+-- El int representa el id del rectángulo.
 data Container     = C {p1x :: Float,
                         p1y :: Float,
                         p2x :: Float,
@@ -17,18 +20,10 @@ data Container     = C {p1x :: Float,
                  
 newtype Containers = Cs [Container] deriving Show
 
--- Representamos un rectángulo de acuerdo a su diagonal "de izquierda a derecha, de abajo hacia arriba". 
--- Es decir, con un vector del cuadrante I.
--- El int representa el id del rectángulo.
-{-data Rectangle = R {p1x :: Float,
-                    p1y :: Float,
-                    p2x :: Float,
-                    p2y :: Float,
-                    rid :: Int} deriving (Show, Eq)
--}
+data Def           = Dp String Polygon Float -- ID, def. del polígono y escalamiento 
+                     | Dc String Container Float deriving Show
 
 -- Tipos para archivos SVG --
 -----------------------------
-newtype DExp = D String deriving Show
 
-type SVGExp  = [DExp]
+type SVGExp  = [String]
