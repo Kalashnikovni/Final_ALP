d205289289731899c0307e97019125109c09656b
Kalashnikovni <denise.marzorati@gmail.com>
2017-04-10 22:57:51 -0300

    Añade pretty printer de poligonos

---
+++ PrettyPrint.hs
@@ -0,0 +1,40 @@
+module PrettyPrint where
+
+-- Módulos propios
+import Common
+
+-- Módulos prestados
+import Text.PrettyPrint.HughesPJ as PJ
+
+printContainer :: Container -> Doc
+printContainer c = text ((nc c) ++ " ") <>
+                   parens (int (rid c)) <>
+                   text " : " <>
+                   PJ.float (abs (p1x c - p2x c)) <>
+                   text " x " <>
+                   PJ.float (abs (p1y c - p2y c))
+
+printContainers cs = printList cs printContainer
+                     
+printMyPointL' :: [MyPoint] -> Doc
+printMyPointL' []     = text "" 
+printMyPointL' (p:mp) = text "(" <>
+                        PJ.float (fst p) <>
+                        text "," <>
+                        PJ.float (snd p) <>
+                        text ")" <>
+                        printMyPointL' mp 
+
+printPolygon :: Polygon -> Doc
+printPolygon po = text (pn po) <>
+                  text " : " <>
+                  brackets (printMyPointL' (p po))
+
+printPolygons ps = printList ps printPolygon
+
+printList :: [a] -> (a -> Doc) -> Doc
+printList [] _     = text ""
+printList (x:xs) f = f x <>
+                     text "\n" <>
+                     printList xs f
+                 
